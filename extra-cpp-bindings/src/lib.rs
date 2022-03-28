3311a260abe90f4e387d6a63d2f4dcc9c4f220c3
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2022-03-28 14:20:19 +0800

    Problem: missing indexed token balance retrieval (fixes #24) (#25)
    Solution:
    - added one BlockScout API endpoint retrival that returns all
    tokens owned by a given address
    (there's no such endpoint in Etherscan/Cronoscan as of now)

--- extra-cpp-bindings/src/lib.rs
+++ extra-cpp-bindings/src/lib.rs
@@ -6,10 +6,11 @@ use ethers_etherscan::{
     },
     Client,
 };
+use serde::{Deserialize, Serialize};
 
 #[cxx::bridge(namespace = "com::crypto::game_sdk")]
 mod ffi {
-    /// Raw transaction details
+    /// Raw transaction details (extracted from Cronoscan/Etherscan API)
     pub struct RawTxDetail {
         /// Transaction hash
         pub hash: String,
@@ -25,6 +26,25 @@ mod ffi {
         pub timestamp: String,
         /// the address of the contract (if no contract, it's an empty string)
         pub contract_address: String,
+    }
+
+    /// Token ownership result detail from BlockScout API
+    #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+    pub struct RawTokenResult {
+        /// how many tokens are owned by the address
+        pub balance: String,
+        /// the deployed contract address
+        #[serde(rename = "contractAddress")]
+        pub contract_address: String,
+        /// the number of decimal places
+        pub decimals: String,
+        /// the human-readable name of the token
+        pub name: String,
+        /// the ticker for the token
+        pub symbol: String,
+        /// the token type (ERC-20, ERC-721, ERC-1155)
+        #[serde(rename = "type")]
+        pub token_type: String,
     }
 
     pub enum QueryOption {
@@ -49,10 +69,14 @@ mod ffi {
             option: QueryOption,
             api_key: String,
         ) -> Result<Vec<RawTxDetail>>;
+        pub fn get_tokens_blocking(
+            blockscout_base_url: String,
+            account_address: String,
+        ) -> Result<Vec<RawTokenResult>>;
     }
 }
 
-use ffi::{QueryOption, RawTxDetail};
+use ffi::{QueryOption, RawTokenResult, RawTxDetail};
 
 /// returns the transactions of a given address.
 /// The API key can be obtained from https://cronoscan.com
@@ -94,6 +118,26 @@ pub fn get_erc721_transfer_blocking(
     rt.block_on(async move {
         get_erc721_transfer_history(&address, &contract_address, option, api_key).await
     })
+}
+
+/// given the BlockScout REST API base url and the account address (hexadecimal),
+/// it will return the list of all owned tokens
+/// (ref: https://cronos.org/explorer/testnet3/api-docs)
+pub fn get_tokens_blocking(
+    blockscout_base_url: String,
+    account_address: String,
+) -> Result<Vec<RawTokenResult>> {
+    let blockscout_url =
+        format!("{blockscout_base_url}?module=account&action=tokenlist&address={account_address}");
+    let resp = reqwest::blocking::get(&blockscout_url)?.json::<RawTokenResponse>()?;
+    Ok(resp.result)
+}
+
+#[derive(Serialize, Deserialize)]
+struct RawTokenResponse {
+    message: String,
+    result: Vec<RawTokenResult>,
+    status: String,
 }
 
 impl From<&NormalTransaction> for RawTxDetail {
@@ -198,4 +242,28 @@ async fn get_erc721_transfer_history(
         .get_erc721_token_transfer_events(token_query, None)
         .await?;
     Ok(transactions.iter().map(|tx| tx.into()).collect())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_get_tokens() {
+        let expected: Vec<RawTokenResult> = serde_json::from_str(r#"[{"balance":"1","contractAddress":"0x1e0edbea442cfeff05ed1b01f0c38ecb768de0e0","decimals":"","name":"NFT Gold","symbol":"Gold","type":"ERC-1155"},{"balance":"36616853110389525899548","contractAddress":"0x27b9c2bd4baea18abdf49169054c1c1c12af9862","decimals":"18","name":"SNAFU","symbol":"SNAFU","type":"ERC-20"},{"balance":"1","contractAddress":"0x57aaaf5a61b6a370f981b7826843694cfa4774e1","decimals":"","name":"Protector","symbol":"サイタマ","type":"ERC-1155"},{"balance":"73000000000000000000","contractAddress":"0x586f8a53c24d8d35a9f49e94d09058560791803e","decimals":"18","name":"NFTOPIUM","symbol":"NTP","type":"ERC-20"},{"balance":"763467280363239051","contractAddress":"0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1","decimals":"18","name":"Wrapped Ether on xDai","symbol":"WETH","type":"ERC-20"},{"balance":"1","contractAddress":"0x90fda259cfbdb74f1804e921f523e660bfbe698d","decimals":"","name":"Unique Pixie","symbol":"UPIXIE","type":"ERC-721"},{"balance":"4","contractAddress":"0x93d0c9a35c43f6bc999416a06aadf21e68b29eba","decimals":"","name":"Unique One","symbol":"UNE","type":"ERC-1155"},{"balance":"3000000000000000000","contractAddress":"0x9c58bacc331c9aa871afd802db6379a98e80cedb","decimals":"18","name":"Gnosis Token on xDai","symbol":"GNO","type":"ERC-20"},{"balance":"1","contractAddress":"0xed1efc6efceaab9f6d609fec89c9e675bf1efb0a","decimals":"","name":"SNAFU","symbol":"SNAFU","type":"ERC-1155"}]"#).expect("parse");
+        let actual = get_tokens_blocking(
+            "https://blockscout.com/xdai/mainnet/api".into(),
+            "0x652d53227d7013f3FbBeA542443Dc2eeF05719De".into(),
+        )
+        .expect("api");
+        assert_eq!(actual.len(), expected.len());
+        for (a, b) in actual.iter().zip(expected.iter()) {
+            // assert_eq!(a.balance, b.balance);
+            assert_eq!(a.contract_address, b.contract_address);
+            assert_eq!(a.decimals, b.decimals);
+            assert_eq!(a.name, b.name);
+            assert_eq!(a.symbol, b.symbol);
+            assert_eq!(a.token_type, b.token_type);
+        }
+    }
 }
