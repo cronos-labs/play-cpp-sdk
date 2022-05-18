0a7fe221f8c1e7c53ffff59301eeff3289159509
Damon Chen <92778185+damoncro@users.noreply.github.com>
2022-05-18 10:13:39 +0800

    Problem: Missing integration test (fixes #23) (#60)
    Solution:
    - Add win-build.yml
    - Add mac-build.yml
    - Add test.yml
    - Add demo on cronoscan and blockscout
    - Format the string correctly

--- demo/main.cc
+++ demo/main.cc
@@ -6,7 +6,98 @@
 #include <thread>
 
 using namespace std;
+using namespace com::crypto::game_sdk;
+using namespace rust;
+
+String getEnv(String key) {
+  String ret;
+  if (getenv(key.c_str()) != nullptr) {
+    ret = getenv(key.c_str());
+  }
+  return ret;
+}
+
+// Read CronoScan api key in env
+const String CRONOSCAN_API_KEY = getEnv("CRONOSCAN_API_KEY");
 
 int main(int argc, char *argv[]) {
-    return 0;
+
+  // CronoScan examples
+  if (CRONOSCAN_API_KEY != "") {
+    Vec<RawTxDetail> txs = get_transaction_history_blocking(
+        "0x7de9ab1e6a60ac7a70ce96d1d95a0dfcecf7bfb7", CRONOSCAN_API_KEY);
+    cout << txs.size() << endl;
+
+    for (Vec<RawTxDetail>::iterator ptr = txs.begin(); ptr < txs.end(); ptr++) {
+      cout << ptr->hash << " ";
+      cout << ptr->to_address << " ";
+      cout << ptr->from_address << " ";
+      cout << ptr->value << " ";
+      cout << ptr->block_no << " ";
+      cout << ptr->timestamp << " ";
+      cout << ptr->contract_address << " " << endl;
+    }
+
+    Vec<RawTxDetail> erc20_txs = get_erc20_transfer_history_blocking(
+        "0xa9b34a4b568e640d5e5d1e6e13101025e1262864",
+        "0x66e428c3f67a68878562e79A0234c1F83c208770",
+        QueryOption::ByAddressAndContract, CRONOSCAN_API_KEY);
+
+    for (Vec<RawTxDetail>::iterator ptr = erc20_txs.begin();
+         ptr < erc20_txs.end(); ptr++) {
+      cout << ptr->hash << " ";
+      cout << ptr->to_address << " ";
+      cout << ptr->from_address << " ";
+      cout << ptr->value << " ";
+      cout << ptr->block_no << " ";
+      cout << ptr->timestamp << " ";
+      cout << ptr->contract_address << " " << endl;
+    }
+
+    Vec<RawTxDetail> erc721_txs = get_erc721_transfer_blocking(
+        "0x668f126b87936df4f9a98f18c44eb73868fffea0",
+        "0xbd6b9a1A0477d64E99F660b7b7C205f4604E4Ff3", QueryOption::ByContract,
+        CRONOSCAN_API_KEY);
+
+    for (Vec<RawTxDetail>::iterator ptr = erc721_txs.begin();
+         ptr < erc721_txs.end(); ptr++) {
+      cout << ptr->hash << " ";
+      cout << ptr->to_address << " ";
+      cout << ptr->from_address << " ";
+      cout << ptr->value << " ";
+      cout << ptr->block_no << " ";
+      cout << ptr->timestamp << " ";
+      cout << ptr->contract_address << " " << endl;
+    }
+  }
+
+  // Blockscout examples
+  Vec<RawTokenResult> tokens_txs =
+      get_tokens_blocking("https://blockscout.com/xdai/mainnet/api",
+                          "0x652d53227d7013f3FbBeA542443Dc2eeF05719De");
+  for (Vec<RawTokenResult>::iterator ptr = tokens_txs.begin();
+       ptr < tokens_txs.end(); ptr++) {
+    cout << ptr->balance << " ";
+    cout << ptr->contract_address << " ";
+    cout << ptr->decimals << " ";
+    cout << ptr->name << " ";
+    cout << ptr->symbol << " ";
+    cout << ptr->token_type << endl;
+  }
+
+  Vec<RawTxDetail> token_transfer_txs = get_token_transfers_blocking(
+      "https://cronos.org/explorer/testnet3/api",
+      "0x841a15D12aEc9c6039FD132c2FbFF112eD355700", "", QueryOption::ByAddress);
+  for (Vec<RawTxDetail>::iterator ptr = token_transfer_txs.begin();
+       ptr < token_transfer_txs.end(); ptr++) {
+    cout << ptr->hash << " ";
+    cout << ptr->to_address << " ";
+    cout << ptr->from_address << " ";
+    cout << ptr->value << " ";
+    cout << ptr->block_no << " ";
+    cout << ptr->timestamp << " ";
+    cout << ptr->contract_address << " " << endl;
+  }
+
+  return 0;
 }
