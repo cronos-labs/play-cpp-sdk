b002b3457bce22bc0be27e270726c4a06a8ab6fa
Damon Chen <damon.chen@crypto.com>
2022-05-20 16:16:22 +0800

    Back to c++11

--- extra-cpp-bindings/build.rs
+++ extra-cpp-bindings/build.rs
@@ -3,7 +3,7 @@ const BRIDGES: &[&str] = &["src/lib.rs"];
 fn main() {
     cxx_build::bridges(BRIDGES)
         .file("src/pay.cc")
-        .flag_if_supported("-std=c++14")
+        .flag_if_supported("-std=c++11")
         .compile("game_sdk_bindings");
 
     for bridge in BRIDGES {
