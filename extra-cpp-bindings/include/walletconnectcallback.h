64b17897c1b958a7a56cb5a54d709ea8404616b8
Damon Chan <92778185+damoncro@users.noreply.github.com>
2022-06-06 09:55:13 +0800

    Problem: using namespace in c++ header (fix #81) (#83)
    Solution:
    - Remove using namespace in headers/sources and keep it consistant

--- extra-cpp-bindings/include/walletconnectcallback.h
+++ extra-cpp-bindings/include/walletconnectcallback.h
@@ -5,44 +5,43 @@
 namespace com {
 namespace crypto {
 namespace game_sdk {
-using namespace rust;
- 
+
     class WalletConnectSessionInfo {
         public:
         /// if the wallet approved the connection
         bool connected;
         /// hex-string(0x...), the accounts returned by the wallet
-        Vec<String> accounts;
+        rust::Vec<rust::String> accounts;
         /// u64, the chain id returned by the wallet
-        String chain_id;
+        rust::String chain_id;
         /// the bridge server URL
-        String bridge;
+        rust::String bridge;
         /// the secret key used in encrypting wallet requests
         /// and decrypting wallet responses as per WalletConnect 1.0
-        /// hex-string(0x...), 32 bytes 
-        String key;
+        /// hex-string(0x...), 32 bytes
+        rust::String key;
         /// this is the client's randomly generated ID
-        String client_id;
+        rust::String client_id;
         /// json, the client metadata (that will be presented to the wallet in the initial request)
-        String client_meta;
+        rust::String client_meta;
         /// uuid, the wallet's ID
-        String peer_id;
+        rust::String peer_id;
         /// json, the wallet's metadata
-        String peer_meta;
+        rust::String peer_meta;
         /// uuid, the one-time request ID
-        String handshake_topic;
+        rust::String handshake_topic;
 
         void set_connected(bool connected) ;
-        void set_accounts(Vec<String> accounts) ;
+        void set_accounts(rust::Vec<rust::String> accounts) ;
         void set_chainid(rust::String chainid) ;
-        void set_bridge(String bridge) ;
-        void set_key(String key) ;
-        void set_clientid(String client_id) ;
-        void set_clientmeta(String client_meta) ;
-        void set_peerid(String client_id) ;
-        void set_peermeta(String client_meta) ;
-        void set_handshaketopic(String handshake_topic) ;
-};  
+        void set_bridge(rust::String bridge) ;
+        void set_key(rust::String key) ;
+        void set_clientid(rust::String client_id) ;
+        void set_clientmeta(rust::String client_meta) ;
+        void set_peerid(rust::String client_id) ;
+        void set_peermeta(rust::String client_meta) ;
+        void set_handshaketopic(rust::String handshake_topic) ;
+};
 std::unique_ptr<WalletConnectSessionInfo> new_walletconnect_sessioninfo();
 
 
